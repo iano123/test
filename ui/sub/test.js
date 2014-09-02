50222792f3c0d81dfd803644eb5fcba986fc2086
iano123 <noreply>
2014-09-02 20:33:06 +0000

    Add module test

---
+++ ui/sub/test.js
@@ -0,0 +1,16 @@
+/**
+ * @module ui/sub/test
+ * @requires montage/core/core
+ */
+var Montage = require("montage/core/core").Montage;
+/**
+ * @class Test
+ * @extends Montage
+ */
+exports.Test = Montage.specialize(/** @lends Test# */ {
+    constructor: {
+        value: function Test() {
+            this.super();
+        }
+    }
+});
