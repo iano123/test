ff9880ce2633e1373eb4d29e0222121a83fb712a
iano123 <noreply>
2014-09-02 20:33:40 +0000

    Add component rtyhrty

---
+++ ui/fghfgh/rtyhrty.reel/rtyhrty.js
@@ -0,0 +1,17 @@
+/**
+ * @module ui/fghfgh/rtyhrty.reel
+ * @requires montage/ui/component
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class Rtyhrty
+ * @extends Component
+ */
+exports.Rtyhrty = Component.specialize(/** @lends Rtyhrty# */ {
+    constructor: {
+        value: function Rtyhrty() {
+            this.super();
+        }
+    }
+});
